8528aa4dbfece4fc90d443e10108e22c54d552ea
jestanley79 <jestanley@my365.bellevue.edu>
2023-01-17 21:11:10 -0500

    Creating folder

--- mongodb_test.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-url = "mongodb+srv://admin:admin@classcluster1.zltsdhr.mongodb.net/pytech"
-client = MongoClient(url)
-db = client.pytech
-
-print("\n\n-- Pytech Collection List --")
-print(db.list_collection_names())
-
-input("\n\n  End of program, press any key to exit... ")
